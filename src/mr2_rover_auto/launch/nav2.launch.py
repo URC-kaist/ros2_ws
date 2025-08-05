522378c2143decd0610c65a4ad890658ad463958
pianojay <159106973+pianojay@users.noreply.github.com>
2025-08-06 00:08:07 +0900

    better definitions for nav2 with behavior tree

--- src/mr2_rover_auto/launch/nav2.launch.py
+++ src/mr2_rover_auto/launch/nav2.launch.py
@@ -12,7 +12,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('use_sim_time', default_value='false'),
 
-        # 0) Lifecycle Manager (automates all transitions)
+        # 0) Lifecycle Manager
         Node(
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
@@ -22,13 +22,12 @@ def generate_launch_description():
             'use_sim_time': LaunchConfiguration('use_sim_time'),
             'autostart': True,
             'node_names': [
+                'bt_navigator',
                 'map_server',
                 'global_costmap',
-                'local_costmap',
                 'planner_server',
                 'controller_server',
-                'smoother_server',
-                'bt_navigator']}]
+                'smoother_server']}]
         ),
 
         # 1) BT Navigator
@@ -40,38 +39,43 @@ def generate_launch_description():
             parameters=[params_file, {'use_sim_time': LaunchConfiguration('use_sim_time')}]
         ),
 
-        # 2) Map Server
+        # 2) Map Server: Publish pre-baked static occupancy /map
         Node(
-            package='nav2_map_server',
-            executable='map_server',
-            name='map_server',
-            output='screen',
-            parameters=[params_file, {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        output='screen',
+        parameters=[params_file, {'use_sim_time': LaunchConfiguration('use_sim_time')}],
         ),
 
-        # 3) Global Costmap
+        # 3) Global Costmap: /map + TraversabilityLayer (or ObstacleLayer) + InflationLayer
         Node(
             package='nav2_costmap_2d',
-            executable='costmap_server',
+            executable='nav2_costmap_2d',
             name='global_costmap',
             output='screen',
             parameters=[params_file, {'use_sim_time': LaunchConfiguration('use_sim_time')}]
         ),
 
-        # 4) Local Costmap
-        Node(
-            package='nav2_costmap_2d',
-            executable='costmap_server',
-            name='local_costmap',
-            output='screen',
-            parameters=[params_file, {'use_sim_time': LaunchConfiguration('use_sim_time')}]
-        ),
-
-        # 5) Planner
+        # 4) Planner: Create path from global costmap; A to B
         Node(
             package='nav2_planner',
             executable='planner_server',
-            name='planner_server',
+            name='planner_a2b_server',
+            output='screen',
+            parameters=[params_file, {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        ),
+        """ Once active, you can send a goal with:
+        ros2 action send_goal /compute_path_to_pose \
+        nav2_msgs/action/ComputePathToPose \
+        "{goal: { header: { frame_id: 'map' }, pose: { position: { x: 1.0, y: 2.0 }, orientation: { w: 1.0 } } }}"
+        """
+
+        # 5) Planner: Create path from global costmap; Coverage
+        Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_c_server',
             output='screen',
             parameters=[params_file, {'use_sim_time': LaunchConfiguration('use_sim_time')}]
         ),
