aa22d3420a567c06c80efff8ab1a1860c8576ed2
Gyungmin Myung <gmmyung@kaist.ac.kr>
2025-08-07 08:40:10 +0900

    rover spawn height

--- src/mr2_rover_description/launch/rover.launch.py
+++ src/mr2_rover_description/launch/rover.launch.py
@@ -67,7 +67,7 @@ def generate_launch_description():
     spawn = Node(
         package="ros_gz_sim",
         executable="create",
-        arguments=["-topic", "robot_description", "-name", "rover", "-z", "5.0"],
+        arguments=["-topic", "robot_description", "-name", "rover", "-z", "0.0"],
     )
 
     gz_bridge = Node(
