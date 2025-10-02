a5779b7e53cde3ab591f810587e0e8bd4901d684
Gyungmin Myung <gmmyung@kaist.ac.kr>
2025-10-02 20:53:02 +0900

    Revert "Merge pull request #22 from URC-kaist/codex/move-and-rename-ak_servo-to-arm_device"
    This reverts commit 121afb704d09bcac152f92531aeae780e1a1c931, reversing
    changes made to a6162d641782d9d00fbf6da2bd56a648479b8edb.

--- src/mr2_devices_ak_servo/src/ak_servo_device.cpp
+++ src/mr2_devices_ak_servo/src/ak_servo_device.cpp
@@ -3,10 +3,10 @@
 #include "hardware_interface/types/hardware_interface_type_values.hpp"
 #include "pluginlib/class_list_macros.hpp"
 
-namespace mr2_devices_arm
+namespace mr2_devices_ak_servo
 {
 
-class ArmDevice : public CanDevice
+class AkServoDevice : public CanDevice
 {
 public:
   void configure(const hardware_interface::ComponentInfo & ji,
@@ -79,6 +79,6 @@ private:
   std::vector<double> pos_, vel_, eff_, cmd_;
 };
 
-}  // namespace mr2_devices_arm
+}  // namespace mr2_devices_ak_servo
 
-PLUGINLIB_EXPORT_CLASS(mr2_devices_arm::ArmDevice, CanDevice)
+PLUGINLIB_EXPORT_CLASS(mr2_devices_ak_servo::AkServoDevice, CanDevice)
