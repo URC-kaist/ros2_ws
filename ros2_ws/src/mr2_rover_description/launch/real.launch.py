a5efbf5557f0fe4b7db533d3c8854c007a20e9d6
Gmmyung Myung <gmmyung@kaist.ac.kr>
2026-01-21 00:21:26 +0900

    fix homing

--- ros2_ws/src/mr2_rover_description/launch/real.launch.py
+++ ros2_ws/src/mr2_rover_description/launch/real.launch.py
@@ -64,8 +64,7 @@ def generate_launch_description():
 
     ros2_control = ros2_control_node(controller_config, robot_description, use_sim_time)
     spawners = controller_spawners(
-        # ["joint_state_broadcaster", "rover_controller", "manipulator_controller"],
-        ["joint_state_broadcaster", "rover_controller"],
+        ["joint_state_broadcaster", "rover_controller", "manipulator_controller"],
         start_after=2.0,
         interval=2.0,
         inactive_controllers=["manipulator_controller"],
