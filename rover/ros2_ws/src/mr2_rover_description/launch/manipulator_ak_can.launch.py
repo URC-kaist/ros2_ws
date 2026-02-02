54ca871d916d2e30f2d8a979306c68914ce30382
Gmmyung Myung <gmmyung@kaist.ac.kr>
2026-02-03 00:57:59 +0900

    add manipulator controller

--- rover/ros2_ws/src/mr2_rover_description/launch/manipulator_ak_can.launch.py
+++ rover/ros2_ws/src/mr2_rover_description/launch/manipulator_ak_can.launch.py
@@ -63,7 +63,7 @@ def generate_launch_description():
 
     ros2_control = ros2_control_node(controller_config, robot_description, use_sim_time)
     spawners = controller_spawners(
-        ["joint_state_broadcaster", "manipulator_controller"],
+        ["joint_state_broadcaster", "manipulator_controller", "gripper_position_controller"],
         start_after=2.0,
         interval=2.0,
         inactive_controllers=["manipulator_controller"],
