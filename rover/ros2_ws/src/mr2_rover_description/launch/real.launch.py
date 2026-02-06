77dae440b7acd62560d2a5a91bb23994faea8dd6
Gmmyung Myung <gmmyung@kaist.ac.kr>
2026-02-06 09:32:55 +0900

    fix arm calibration values

--- rover/ros2_ws/src/mr2_rover_description/launch/real.launch.py
+++ rover/ros2_ws/src/mr2_rover_description/launch/real.launch.py
@@ -65,7 +65,7 @@ def generate_launch_description():
     ros2_control = ros2_control_node(controller_config, robot_description, use_sim_time)
     spawners = controller_spawners(
         # ["joint_state_broadcaster", "rover_controller", "manipulator_controller", "gripper_position_controller"],
-        ["joint_state_broadcaster", "manipulator_controller", "gripper_position_controller"],
+        ["joint_state_broadcaster", "rover_controller", "manipulator_controller"],
         start_after=2.0,
         interval=2.0,
         inactive_controllers=["manipulator_controller"],
